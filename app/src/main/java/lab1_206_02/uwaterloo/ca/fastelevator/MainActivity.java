e0ad183134d27db46a98dc1e2ea4b3fc159ee400
Zemann <zemann.schps@gmail.com>
2017-05-12 20:15:10 -0400

    Initial commit

---
+++ app/src/main/java/lab1_206_02/uwaterloo/ca/fastelevator/MainActivity.java
@@ -0,0 +1,15 @@
+package lab1_206_02.uwaterloo.ca.fastelevator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
